6cbb02155934954536ebba619f79a90b2e6125d1
user7210unix <user7210@proton.me>
2025-01-29 13:05:39 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Clone the repository
 git clone https://github.com/user7210unix/info-fetch
 
-# Change permissions to make 'info' executable
 chmod +x info-fetch/info
 
-# Copy the 'info' directory to /usr/bin/
 sudo cp -r info-fetch/info /usr/bin/
 
-# Optionally clean up
 rm -rf info-fetch
+
+clear
+
+info
 
 echo "Installation complete! You can now use 'info' from anywhere."
